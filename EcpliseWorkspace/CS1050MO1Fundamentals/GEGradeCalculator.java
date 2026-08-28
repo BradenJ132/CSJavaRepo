bef932ef28cdbd594ce8db2689a3f87c25e900f4
BradenJ132 <bjbraden18@gmail.com>
2026-08-28 11:34:06 -0600

    First project + grade calculator code

---
+++ EcpliseWorkspace/CS1050MO1Fundamentals/GEGradeCalculator.java
@@ -0,0 +1,32 @@
+/**
+ * 
+ */
+
+/**
+ * 
+ */
+
+public class GEGradeCalculator {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+		final double Weight_Class_Participation = 15;
+		final double Weight_Guided_Explorations = 20;
+		final double Weight_Quizzes = 25;
+		final double Weight_Projects = 20;
+		final double Weight_Final_Demonstation = 20;
+		
+		double gradeclassparticipation = 74;
+		double gradeguidedexploration = 85.3;
+		double gradequizzes = 91.25;
+		double gradeprojects = 72.3;
+		double finaldemonstation = 91.74;
+	
+		double Percentageweight = gradeclassparticipation * Weight_Class_Participation;
+		System.out.print(Percentageweight);
+	}
+
+}
