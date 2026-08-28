50d16f206be5487726b3fb922deb94485fac8863
BradenJ132 <bjbraden18@gmail.com>
2026-08-28 17:03:45 -0600

    Update GEGradeCalculator.java

--- EcpliseWorkspace/CS1050MO1Fundamentals/GEGradeCalculator.java
+++ EcpliseWorkspace/CS1050MO1Fundamentals/GEGradeCalculator.java
@@ -6,11 +6,14 @@
  * 
  */
 
-public class GEGradeCalculator {
 
-	/**
-	 * @param args
-	 */
+
+	//Braden Jones
+	//CS1050 
+	//GEMO1
+	//this program calculates a final grade for this class based on the category weights.
+	
+public class GEGradeCalculator {
 	public static void main(String[] args) {
 		
 		final double Weight_Class_Participation = 15;
@@ -19,10 +22,10 @@ public class GEGradeCalculator {
 		final double Weight_Projects = 20;
 		final double Weight_Final_Demonstation = 20;
 		
-		double gradeclassparticipation = 74;
+		double gradeclassparticipation = 79;
 		double gradeguidedexploration = 85.3;
 		double gradequizzes = 91.25;
-		double gradeprojects = 72.3;
+		double gradeprojects = 72.325;
 		double finaldemonstation = 91.74;
 	
 		double Percentageweightclassparticipation = gradeclassparticipation * Weight_Class_Participation;
@@ -31,9 +34,12 @@ public class GEGradeCalculator {
 		double Percentageweightprojects = gradeprojects * Weight_Projects;
 		double Percentageweightfinaldemonstration = finaldemonstation * Weight_Final_Demonstation;
 		
-		double totalweight = Weight_Class_Participation * Weight_Guided_Explorations * Weight_Quizzes * Weight_Projects * Weight_Final_Demonstation;
- 
-	 System.out.print(totalweight);
+		double totalweight = Weight_Class_Participation + Weight_Guided_Explorations + Weight_Quizzes + Weight_Projects + Weight_Final_Demonstation;
+		double totalpercentageweight = Percentageweightclassparticipation + Percentageweightguidedexploration + Percentageweightquizzes + Percentageweightprojects +  Percentageweightfinaldemonstration;
+	
+		double finalgrade = totalpercentageweight / totalweight;
+		
+		System.out.printf("final grade: " + finalgrade); 
 	}
 
 }
