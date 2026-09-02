bfc020334f10a47424095b9d086af36ae70abe66
BradenJ132 <bjbraden18@gmail.com>
2026-09-02 12:46:19 -0600

    python game changes

--- EcpliseWorkspace/Game Test/Gametest.py
+++ EcpliseWorkspace/Game Test/Gametest.py
@@ -1,34 +1,46 @@
 #import pygame  
 import pygame
+from pygame import key
+
 #ininitializes pygame
 pygame.init()
 #creates window
 screen = pygame.display.set_mode((1920, 1080))
 #import the image/ converts it into a surface
-frogimg = pygame.image.load("BlueFrog.jpg").convert()
-#this is supposed to wokr but it deletes any color that is white 
+frogimg = pygame.image.load("Frogimg.gif").convert()
+#this is supposed to work but it deletes any color that is white 
 frogimg.set_colorkey((255, 255, 255))
+
 
 #this says when it is running x = 0 
 #the clock is defining the tick rate/fps
 #i will likely adjust with delta time later
-running = True 
-x = 0
 clock = pygame.time.Clock()
-# while the prgoma is running fill the window with black and place the surface/frog as that point
-#also while it is running, move in the positive x direction 0.1 per tick 
-while running:
-    screen.fill((0, 0, 0))
-    screen.blit(frogimg, (x, 20))
+x = 0
+y = 0
+running = True 
 
-    x += 0.1
-#this is saying when the event type/ x key i bielive it is, is pressed then quit the game/turn running to false
-    for event in pygame.event.get():
+# while the program is running fill the window with black and place the surface/frog as that point
+#also while it is running, move in the positive x direction 0.1 per tick 
+screen.fill((0, 0, 0))
+screen.blit(frogimg,(x, y))
+
+while running:
+    keys = pygame.key.get_pressed
+    if keys == pygame[key_w]:
+            x += 3
+
+for event in pygame.event.get():
         if event.type == pygame.QUIT:
-            running = False 
+            running = False
+
+
+
+#this is saying when the event type/ x key i beilive it is, is pressed then quit the game/turn running to false
+    
 
 # this actually displays the surfaces on the screen
-    pygame.display.flip()
+pygame.display.flip()
 # this defines the fps/tickrate
 clock.tick(60) 
 pygame.quit()
--- VsCodeProjects/python game.py
+++ VsCodeProjects/python game.py
@@ -4,25 +4,3 @@ import pygame
 pygame.init()
 screen = pygame.display.set_mode((1920, 1080))
 
-frogimg = pygame.image.load("BlueFrog.jpg").convert()
-frogimg.set_colorkey((255, 255, 255))
-
-running = True 
-x = 0
-clock = pygame.time.Clock()
-
-while running:
-    screen.fill((0, 0, 0))
-    screen.blit(frogimg, (x, 20))
-
-    x += 0.1
-
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False 
-
-
-    pygame.display.flip()
-
-clock.tick(60) 
-pygame.quit()
