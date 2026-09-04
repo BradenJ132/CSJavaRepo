bdb24a01bf48b227a7f1c69bda3d03a45f6d9c74
BradenJ132 <bjbraden18@gmail.com>
2026-09-04 09:02:22 -0600

    Update Gametest.py

--- EcpliseWorkspace/Game Test/Gametest.py
+++ EcpliseWorkspace/Game Test/Gametest.py
@@ -14,7 +14,8 @@ class game:
         self.frogimg = pygame.image.load("Frogimg.gif").convert_alpha()
     
         self.frogimg_pos = [100, 200]
-
+        self.ymovement = [False, False]
+        self.xmovement = [False, False]
 
      
 
@@ -22,14 +23,40 @@ class game:
     def run(self):
         running = True
         while running:
+
+#the first part of the code say, get the position then you are saying get the ymovement based on the operation. it basically is stating 
+# if the first value[1] is greater than [0] then move down because postive y is in the downwards direction and if [1]<[0] move up
+            self.frogimg_pos[1] += self.ymovement[1] - self.ymovement[0] 
+            self.frogimg_pos[0] += self.xmovement[1] - self.xmovement[0]
             self.screen.blit(self.frogimg, self.frogimg_pos)
 
-            #gets user input so any input coming from the keyboard, files, or windows in general must have this command
+          
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit() #this closes pygame. (both commands are needed)
                     sys.exit() #this codes the X in the top right to close the window.
-                    
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_w:
+                        self.ymovement[0] = True
+                    if event.key == pygame.K_s:
+                        self.ymovement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_w:
+                        self.ymovement[0] = False
+                    if event.key == pygame.K_s:
+                        self.ymovement[1] = False
+
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_a:
+                        self.xmovement[0] = True
+                    if event.key == pygame.K_d:
+                        self.xmovement[1] = True
+                if event.type == pygame.KEYUP:
+                    if event.key == pygame.K_a:
+                        self.xmovement[0] = False
+                    if event.key == pygame.K_d:
+                        self.xmovement[1] = False                
+    
 
             pygame.display.flip()
             self.clock.tick(60)  
