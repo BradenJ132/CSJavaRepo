c2f43a2e787388de563a04c4c46463c83489f299
BradenJ132 <bjbraden18@gmail.com>
2026-09-17 17:29:08 -0600

    updated concert seating plus made new project for mod 3

--- EcpliseWorkspace/Mo2_oop_and_Design_Structure/ConcertSeatingGEMO2.java
+++ EcpliseWorkspace/Mo2_oop_and_Design_Structure/ConcertSeatingGEMO2.java
@@ -8,9 +8,61 @@ public class ConcertSeatingGEMO2
 System.out.print("please Enter your Section Character: ");
 char sectioncharacter = input.next().charAt(0);
 sectioncharacter = Character.toUpperCase(sectioncharacter);
-System.out.print(sectioncharacter);
+	
 		
-		
+while (sectioncharacter != ('F') && sectioncharacter != ('C') && sectioncharacter != ('S') && sectioncharacter != ('U'))
+			{
+				 System.out.print("please enter a valid Section: ");
+				sectioncharacter = input.next().charAt(0);
+				sectioncharacter = Character.toUpperCase(sectioncharacter);
+				}
+	
+	if (sectioncharacter == ('F'))
+			{
+		System.out.println("You are in section Foxtrot(f)");
+			}
+			
+	else if (sectioncharacter == ('C'))
+	{
+System.out.println("You are in section Charlie(C)");
+	}	
+	
+	
+	else if (sectioncharacter == ('S'))
+	{
+System.out.println("You are in section Sierra(S)");
+	}
+	
+	else 
+	{
+System.out.println("You are in section Uniform(U)");
+	}
+	
+	System.out.println("Please enter your Row number: " );
+	int rownumber = input.nextInt();
+	while (rownumber > (60) || rownumber < (1))
+	{
+		System.out.println("please enter a valid Row Number: " );
+		rownumber = input.nextInt();
+	}
+	
+	if (rownumber >= 1 && rownumber <= 15)
+	{
+		System.out.println("your ticket costs $450");
+	}
+	
+	else if (rownumber > 15 && rownumber <= 30)
+	{
+		System.out.println("your ticket costs $300");
+	}
+	else 
+	{
+		System.out.println("your ticket costs $200");
+	}
+	
+	System.out.println("your seat is located at "+ sectioncharacter + rownumber); 	
+	input.close();
+	
 	}
 
 }
